72c06ca2886596768f929bf8429cd20b5a9e73ae
Octavio <nboctavio@gmail.com>
2018-05-11 09:57:12 +0200

    l

---
+++ week10/src/test/java/com/greenfox/clothing/clothing/ClothingApplicationTests.java
@@ -0,0 +1,20 @@
+package com.greenfox.clothing.clothing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+/*
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClothingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+ */
